2e7d3fa40d446f5902c5c86b4e4bfa4f076dc54d
Suyash Patil <suyashpatil532001@gmail.com>
2021-04-17 13:41:45 +0530

    doctor functionality added with redirect

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase'
 
-const Doctor = () => {
+const Doctor = ({ history, user, setUser }) => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      if (user.email !== "pullstackdevelopers@gmail.com") {
+        history.push("/")
+      }
+      if (user.email === "pullstackdevelopers@gmail.com") {
+        db.collection("questions").orderBy("timestamp", "desc")
+          .onSnapshot((snapshot) => {
+            setData(snapshot.docs.map(doc => ({
+              id: doc.id,
+              userData: doc.data()
+
+            })))
+          })
+      }
+    }
+  }, [user])
+
   return (
     <div>
-
+      {
+        data.map(({ userData, id }) => (
+          <div>
+            <p>{userData.email}</p>
+            <p>{userData.dailyDiary}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
