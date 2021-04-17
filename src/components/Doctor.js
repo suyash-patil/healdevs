6fdf7158cfc31d235eb57ec4d918fd1eb830cfb9
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2021-04-17 15:07:33 +0530

    Doctor Page updated

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../firebase'
+import './doctor.css'
 
 const Doctor = ({ history, user, setUser }) => {
 
@@ -25,14 +26,22 @@ const Doctor = ({ history, user, setUser }) => {
 
   return (
     <div>
-      {
-        data.map(({ userData, id }) => (
-          <div>
-            <p>{userData.email}</p>
-            <p>{userData.dailyDiary}</p>
-          </div>
-        ))
-      }
+      <div class="container-fluid doctor">
+
+        <div className="doctor-heading"><h2>This is the doctor name</h2></div>
+
+        {
+          data.map(({ userData, id }) => (
+            <div class="doctor-box">
+              <div class="patient-details-box">
+                <p class="email">{userData.email}</p>
+                <p class="diary">{userData.dailyDiary}</p>
+              </div>
+            </div>
+          ))
+        }
+        <div className="doctor-ending"></div>
+      </div>
     </div>
   )
 }
